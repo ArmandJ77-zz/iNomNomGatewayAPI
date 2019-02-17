6d549ba7280d818bbc26abfef9cbe78ded8c6231
ArmandJ77 <armandjordaan@live.co.za>
2019-02-17 12:26:52 +0200

    Added mappings

--- GatewayAPI/Domain/Employees/Mapping/EmployeesMapping.cs
+++ GatewayAPI/Domain/Employees/Mapping/EmployeesMapping.cs
@@ -1,7 +1,8 @@
-﻿using System;
+﻿using AutoMapper;
+using Domain.Employees.DTO;
+using Domain.User.DTO;
+using ExternalPythonService.Domain.Employees.DTO;
 using System.Collections.Generic;
-using System.Text;
-using AutoMapper;
 
 namespace Domain.Employees.Mapping
 {
@@ -9,7 +10,22 @@ namespace Domain.Employees.Mapping
     {
         public EmployeesMapping()
         {
+            
+            CreateMap<position, PositionDto>()
+                .ReverseMap();
 
+            CreateMap<ExternalPythonService.Domain.Employees.DTO.User, UserDto>()
+                .ReverseMap();
+
+            CreateMap<employee, EmployeeDto>()
+                .ReverseMap();
+
+            CreateMap<List<employee>, List<EmployeeDto>>()
+                .AfterMap((s, d, context) =>
+                {
+                    s.ForEach(x => d.Add(context.Mapper.Map<employee, EmployeeDto>(x)));
+                })
+                .ReverseMap();
         }
     }
 }
